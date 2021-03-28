1138cb58c1ff588533f2e23b3d2f9858c6009eac
joaopaulopereirax <joaopaulop.dev@gmail.com>
2021-03-27 21:01:01 -0300

    fix: Clearing the code

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+//Components
+import Header from '../../components/Header'
+
+
+export default function Home(){
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
