3b5172fce6161d494e8dabf6d94c762cbf019dff
joaopaulopereirax <joaopaulop.dev@gmail.com>
2021-03-29 11:46:02 -0300

    add: Creating header in mobile version

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,48 @@
 import React from 'react'
+import logo from '../../assets/images/logo.png'
+import alignJustify from '../../assets/images/align-justify.svg'
+import './styles.css'
 
 
+export default function Header(){
+  // const selec = (el) =>{document.querySelector(el)}
+  // const [visible,setVisible] = useState(true);
+  
+  // const hide = () =>{
+  //   selec(".menu").style.display = "none";
+  // }
 
-const Header = () =>{
+  // function show() {
+  //   selec(".menu").style.display = "block";
+  // }
+
+  // function hideShow() {
+  //   if (visible) {
+  //     show();
+  //   } else {
+  //     hide();
+  //   }
+  // }
+
+  // hideShow()
   return(
     <header>
-      <h1>Aqui vai nosso Header</h1>
+      <div className="logo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className="menuimg">
+        <img src={alignJustify} alt="Menu"/>
+      </div>
+      <div className="menu">
+        <ul>
+          <li><a href="/">HOME</a></li>
+          <li><a href="/">CARDÁPIO</a></li>
+          <li><a href="/">CONTATO</a></li>
+        </ul>
+      </div>
+      <div className="car">CM</div>
     </header>
   )
 }
 
-export default Header
+ 
