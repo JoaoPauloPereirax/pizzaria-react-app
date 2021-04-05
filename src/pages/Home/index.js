d81aa564c9e1ff58821228ccd217f2c3875043b8
joaopaulopereirax <joaopaulop.dev@gmail.com>
2021-04-05 19:36:29 -0300

    add: Navbar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 //Components
-import Header from '../../components/Header'
-
+import Navbar from '../../components/Navbar'
 
 export default function Home(){
   return (
     <div>
-      <Header/>
+      <Navbar/>
+      {/* <h2>Provou, se apaixonou! Quem pede não se arrepende e se você ainda não comeu não faz ideia do que está perdendo. A pizza chega aí quentinha, feita com muito amor e ingredientes de altíssima qualidade. Prove aí que eu sei que vai querer de novo. </h2> */}
     </div>
   )
 }
