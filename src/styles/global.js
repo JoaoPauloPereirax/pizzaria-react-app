31cbe0b3575b1147dea4d09ce8265e068601e507
joaopaulopereirax <joaopaulop.dev@gmail.com>
2021-04-05 19:26:38 -0300

    feat: Clearing the code

---
+++ src/styles/global.js
@@ -0,0 +1,17 @@
+import {createGlobalStyle} from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap');
+  *{
+    box-sizing: border-box;
+    font-family: "Roboto", sans-serif;
+  }
+
+  body{
+    margin: 0;
+    padding: 0;
+  }
+
+`
+
+export default GlobalStyles
---
+++ src/styles/theme.js
@@ -0,0 +1,7 @@
+const theme = {
+  colors: {
+    background: '#dedede',
+  }
+}
+
+export default theme
