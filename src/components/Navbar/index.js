bbfb123c9ccfeadc53433fd3eee5305663b3796b
joaopaulopereirax <joaopaulop.dev@gmail.com>
2021-04-05 23:34:03 -0300

    add: styling navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import {Nav,NavContainer,NavLogo} from './NavbarElements'
+import {FaBars} from 'react-icons/fa'
+import {Nav,NavContainer,NavLogo,MobileIcon,NavItem,Logo,NavLinks,NavMenu} from './NavbarElements'
+import logo from '../../assets/images/logo.png'
 
 export default function Navbar (){
      return(
           <Nav>
                <NavContainer>
-                    <NavLogo to='/' >Pizza</NavLogo>
+                    <NavLogo to='/' ><Logo src={logo} /></NavLogo>
+                    <MobileIcon>
+                         <FaBars/>
+                    </MobileIcon>
+                    <NavMenu>
+                         <NavItem>
+                              <NavLinks to="about">About</NavLinks>
+                              <NavLinks to="about">About 2</NavLinks>
+                         </NavItem>
+                    </NavMenu>
                </NavContainer>
           </Nav>
      )
